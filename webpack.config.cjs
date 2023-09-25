4711b8181140d456b2bbce0c07a279c188347b2c
olepiob <ashley@digitalhippie.xyz>
2023-09-25 20:00:55 +0200

    webpack se poes

---
+++ webpack.config.cjs
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+// import path from "path"
+
+module.exports = {
+  mode: "development",
+  entry: { index: path.resolve(__dirname, "src/scripts/index.ts") },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "public/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "public/assets",
+          to: "assets",
+        },
+      ],
+    }),
+  ],
+};
