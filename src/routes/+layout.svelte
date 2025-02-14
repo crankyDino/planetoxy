ef395f0d587dbd71e8cacea6d307608a4641478e
skywizard <bamodise+git@protonmail.com>
2025-02-14 21:23:36 +0200

    moving to tailwind v4

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,17 @@
 <script lang="ts">
-  import "../styles/style.css";
+  import "../app.css";
   import Navbar from "../lib/components/navbar/navbar.svelte";
-  // @ts-ignore
   import Sidebar from "../lib/components/sidebar/sidebar.svelte";
   import Footer from "$lib/components/footer/footer.svelte";
-
-  // interface Props {
-  //   children: null | import("svelte").Snippet;
-  // }
-  // let { children }: Props = $props();
+  interface Props {
+    children: null | import("svelte").Snippet;
+  }
+  let { children }: Props = $props();
 </script>
 
 <Navbar />
 
-<div class="h-100% min-h-[100vh]">
-  <!-- {@render children()} -->
-  <slot />
+<div class="h-100% min-h-[100vh] bg-black-dh bg-light">
+  {@render children!()}
 </div>
 <Footer />
