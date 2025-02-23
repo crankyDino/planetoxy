f110cffe4de7df61e4183e46dcd73ed9d3f960f5
skywizard <bamodise+git@protonmail.com>
2025-02-23 21:34:35 +0200

    started making things look nice on project>article

--- src/routes/(pages)/projects/[article]/+page.svelte
+++ src/routes/(pages)/projects/[article]/+page.svelte
@@ -5,7 +5,7 @@
 <Sidebar />
 
 <section class="pt-6 pl-16">
-  <div class="grid grid-flow-col gap-x-4 align-">
+  <div class="grid grid-flow-col gap-x-4">
     <div class="row-span-12 col-span-6 col-start-1 row-start-1">
       <div class="pb-2">
         <img
@@ -45,10 +45,30 @@
     >
       A Thing I Did
     </h1>
-    <p class="row-span-2 col-span-3 text-dh-white row-start-9">
+    <p class="row-span-2 col-span-3 font-space-mono text-dh-white row-start-9">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
       reprehenderit us maxime modi corrupti perferendis ipsum, laudantium ea.
       Lorem ipsum dolor sit amet consectetur adipisicing elit. laudantium ea.
     </p>
+  </div>
+
+  <div class="grid grid-flow-col gap-x-4 pt-6">
+    <div class="row-span-3">
+      <h2 class="text-dh-orange text-4xl font-bold font-pokemon-classic">
+        Tools Used
+      </h2>
+      <ul class="font-space-mono text-dh-white text-2xl pt-3">
+        <li>> Adobe Photoshop</li>
+        <li>> Adobe Illustrator</li>
+        <li>> Canon M40 MII</li>
+      </ul>
+    </div>
+    <div class="scale-[3.2] rounded-xs relative top-[100%] overflow-hidden w-[135px] h-[92px]">
+      <img
+        src="https://media.digitalhippie.xyz/beat%20tape%20cover.jpg"
+        alt=""
+        class="z-10 h-auto w-[400px]"
+      />
+    </div>
   </div>
 </section>
