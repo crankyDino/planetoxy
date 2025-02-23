d693d49cf17f2e65f9a909ea55eefb5a2592a31b
skywizard <bamodise+git@protonmail.com>
2025-02-23 20:27:12 +0200

    Vavi made me do it Added projects article template Added project article styles fighting with tailwind @utility

--- src/routes/(pages)/projects/+page.svelte
+++ src/routes/(pages)/projects/+page.svelte
@@ -4,49 +4,51 @@
 
 <Sidebar />
 
-<section class="grid m-3 mt-6 mx-auto w-[88%]">
-  <div
-    class="border-[.15em] border-dh-orange h-36 rounded-md px-3 py-2 col-start-1 row-start-1"
-  >
-    <div class="flex flex-col grid-rows-12">
-      <h3 class="font-hanuman text-dh-orange text-3xl font-extrabold">
-        Did a thing
-      </h3>
-      <p
-        class="font-quirkyrobot text-dh-light-gray/50"
-        style="letter-spacing: .2em;"
+<section class="grid md:grid-cols-2 gap-8 gap-y-12 m-3 mt-6 mx-auto w-[88%]">
+  {#each [1, 2, 3, 4] as i}
+    <a id="projectCard" href="projects/a-thing" class="overflow-[unset] grid">
+      <div
+        class="col-span-9 border-[.15em] border-dh-orange h-36 rounded-md px-3 py-2 col-start-1 row-start-1"
       >
-        20/04/2023
-      </p>
-    </div>
-
-    <div
-      class="font-space-mono text-dh-gray flex flex-col text-[.7em] border-l-[.5em] border-dh-orange pl-1"
-    >
-      <p class="">Project > Website</p>
-      <p class="">Client > Rareclub</p>
-      <p class="">Date > 2019</p>
-    </div>
-  </div>
-
-  <div
-    class="col-start-1 scale-[1.3] row-start-1 rounded-lg justify-self-end overflow-hidden w-[135px] h-[92px] relative top-[42%] left-[-1%] mix-blend-color"
-  >
-    <img
-      class="z-10 border border-dh-orange bottom-[50%] relative h-auto w-[400px]"
-      style="clip-path: inset(15px 0px);"
-      src="https://media.digitalhippie.xyz/beat%20tape%20cover.jpg"
-      alt="a thing"
-    />
-  </div>
-  <div
-    class="col-start-1 scale-[1.3] row-start-1 rounded-lg justify-self-end overflow-hidden w-[135px] h-[92px] relative top-[52%] left-[-8%]"
-  >
-    <img
-      class="z-10 border border-dh-orange bottom-[50%] relative h-auto w-[400px]"
-      style="clip-path: inset(15px 0px);"
-      src="https://media.digitalhippie.xyz/beat%20tape%20cover.jpg"
-      alt="a thing"
-    />
-  </div>
+        <div class="flex flex-col grid-rows-12">
+          <h3 class="font-hanuman text-dh-orange text-3xl font-extrabold">
+            Did a thing
+          </h3>
+          <p
+            class="font-quirkyrobot text-dh-light-gray/50"
+            style="letter-spacing: .2em;"
+          >
+            20/04/2023
+          </p>
+        </div>
+        <div
+          class="font-space-mono text-dh-gray flex flex-col text-[.7em] border-l-[.5em] border-dh-orange pl-1"
+        >
+          <p>-Website</p>
+          <p>-Rareclub</p>
+          <p>-2019</p>
+        </div>
+      </div>
+      <div
+        class="col-start-7 col-span-3 scale-[1.3] row-start-1 rounded-lg justify-self-end overflow-hidden w-[135px] h-[92px] relative top-[4rem] left-[-1%] mix-blend-color"
+      >
+        <img
+          class="z-10 border border-dh-orange bottom-[50%] relative h-auto w-[400px]"
+          style="clip-path: inset(15px 0px);"
+          src="https://media.digitalhippie.xyz/beat%20tape%20cover.jpg"
+          alt="a thing"
+        />
+      </div>
+      <div
+        class="col-start-6 col-span-3 scale-[1.3] row-start-1 rounded-lg justify-self-end overflow-hidden w-[135px] h-[92px] relative top-[5rem]"
+      >
+        <img
+          class="z-10 border border-dh-orange bottom-[50%] relative h-auto w-[400px]"
+          style="clip-path: inset(15px 0px);"
+          src="https://media.digitalhippie.xyz/beat%20tape%20cover.jpg"
+          alt="a thing"
+        />
+      </div>
+    </a>
+  {/each}
 </section>
---
+++ src/routes/(pages)/projects/[article]/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import Sidebar from "$lib/components/sidebar/sidebar.svelte";
+</script>
+
+<Sidebar />
+
+<section class="pt-6 pl-16">
+  <div class="grid grid-flow-col gap-x-4 align-">
+    <div class="row-span-12 col-span-6 col-start-1 row-start-1">
+      <div class="pb-2">
+        <img
+          src="https://media.digitalhippie.xyz/beat%20tape%20cover.jpg"
+          alt=""
+          class="h-96"
+        />
+        <div class="grid grid-flow-col grid-cols-4 py-1 gap-0">
+          {#each [1, 1] as tag}
+            <p
+              class=" text-dh-white w-fit tracking-wider text-sm font-space-mono font-thin min-w-[11%] overflow-hidden text-nowrap text-ellipsis"
+            >
+              chill
+            </p>
+          {/each}
+        </div>
+      </div>
+
+      <ul class="grid grid-flow-col grid-cols-6 gap-x-4">
+        {#each [1, 1, 1] as tag}
+          <li>
+            <a target="_blank" href="https://digitalhippie.xyz">
+              <img
+                style="max-width: 4rem; width: 2.2rem;"
+                src="../assets/vectors/dh_logo.svg"
+                alt="digital hippie"
+                class="filter--white"
+              />
+            </a>
+          </li>
+        {/each}
+      </ul>
+    </div>
+
+    <h1
+      class="col-span-8 font-bold text-dh-white font-pokemon-classic text-5xl"
+    >
+      A Thing I Did
+    </h1>
+    <p class="row-span-2 col-span-3 text-dh-white row-start-9">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+      reprehenderit us maxime modi corrupti perferendis ipsum, laudantium ea.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. laudantium ea.
+    </p>
+  </div>
+</section>
