d0372c71e834e407eea2bfbe9ac73c69f1afad2b
skywizard <modiseab+github@gmail.com>
2025-03-10 23:12:38 +0200

    updated graph ql model

--- src/lib/server/sanity.service/sanity.service.ts
+++ src/lib/server/sanity.service/sanity.service.ts
@@ -7,10 +7,10 @@ const client = createClient({
     dataset: 'production',
     useCdn: true, // set to `false` to bypass the edge cache
     apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
-    token:PUBLIC_SANITY_API_KEY
+    token: PUBLIC_SANITY_API_KEY
 })
 
 export async function fetchMedia(refId: string) {
-    const query = `*[_type=='frame' && _id == '${refId}']{title, type, contentUrl, description}`;
+    const query = `*[_type=='graphic' && _id == '${refId}']{title, type, contentUrl, description}`;
     return await client.fetch(query);
 }
