6b8da84be1973a8d25c6b6c1e3becdd8b90ebfdc
skywizard <modiseab+github@gmail.com>
2025-03-18 23:19:00 +0200

    can't beleive i got this to work

--- src/routes/(pages)/portfolio/+page.svelte
+++ src/routes/(pages)/portfolio/+page.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
+  import "./portfolio.css";
   import Sidebar from "$lib/components/sidebar/sidebar.svelte";
 </script>
 
 <Sidebar />
 
+<div class="marquee absolute flex flex-row top-48">
+  <h1
+    class="marquee-text font-pokemon-classic text-9xl text-dh-white font-extrabold invert-100 mix-blend-exclusion"
+    style="    -webkit-text-stroke: 2px #dddddd;"
+  >
+    RARECLUB
+  </h1>
+</div>
+
 <div class="grid grid-flow-row m-auto w-6/7">
-  <div class="grid row-span-8 col-start-1">
+  <div class="relative grid col-span-8 row-span-8 col-start-1 row-start-1">
     <div
       class="col-start-1 col-span-3 row-start-1 rounded-lg justify-self-start overflow-hidden relative left-[-1%]"
     >
@@ -27,8 +37,10 @@
       />
     </div>
   </div>
-  <div class="grid col-span-4 col-start-2">
-    <div class="flex flex-col">
+  <div
+    class="relative grid col-span-5 row-span-8 col-start-5 row-start-1 pl-8 pb-12"
+  >
+    <div class="flex flex-col row-start-2">
       <h4
         class="font-space-mono text-dh-orange text-3xl font-bold"
         style="letter-spacing: .4em;"
@@ -39,8 +51,25 @@
       <p class="font-space-mono text-dh-dark-gray text-xl">2020</p>
     </div>
     <a
-      class="flex items-center justify-center btn bg-dh-orange border-[3px] border-dh-orange text-dh-white"
+      class="flex col-start-12 row-start-12 items-center justify-center self-end btn bg-dh-orange border-[3px] border-dh-orange text-dh-white"
       href="http://">View</a
     >
   </div>
+</div>
+
+<div class="flex flex-row w-full justify-center gap-24">
+  <button class="">
+    <img
+      class="headshot w-10 relative z-[2] rotate-180"
+      src="/assets/vectors/icons/arrow-right.svg"
+      alt="left navigation arrow button "
+    />
+  </button>
+  <button class="">
+    <img
+      class="headshot w-10 relative z-[2]"
+      src="/assets/vectors/icons/arrow-right.svg"
+      alt="right navigation arrow button "
+    />
+  </button>
 </div>
