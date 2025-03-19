d892a06636b7b52298590948270b0f0e8ee257c6
skywizard <modiseab+github@gmail.com>
2025-03-19 20:56:21 +0200

    i tried

--- src/routes/(pages)/portfolio/+page.svelte
+++ src/routes/(pages)/portfolio/+page.svelte
@@ -5,7 +5,7 @@
 
 <Sidebar />
 
-<div class="marquee absolute flex flex-row top-48">
+<div class="marquee absolute flex flex-row top-48 w-full">
   <h1
     class="marquee-text font-pokemon-classic text-9xl text-dh-white font-extrabold invert-100 mix-blend-exclusion"
     style="    -webkit-text-stroke: 2px #dddddd;"
@@ -20,17 +20,17 @@
       class="col-start-1 col-span-3 row-start-1 rounded-lg justify-self-start overflow-hidden relative left-[-1%]"
     >
       <img
-        class=" border border-dh-orange relative h-auto w-[400px]"
+        class=" border border-dh-orange relative h-auto w-[400px] xl:w-[28vw]"
         style="clip-path: inset(15px 0px);"
         src="https://media.digitalhippie.xyz/beat%20tape%20cover.jpg"
         alt="a thing"
       />
     </div>
     <div
-      class="col-start-3 col-span-3 row-start-1 rounded-lg justify-self-start overflow-hidden w-fit h-[15em] relative top-[10rem] mix-blend-hard-light invert-75 scale-125"
+      class="col-start-3 col-span-3 row-start-1 rounded-lg justify-self-start overflow-hidden w-fit xl:w-[28vw] h-[15em] xl:h-[33vh] relative top-[10rem] xl:top-[20rem]  mix-blend-hard-light invert-75 scale-125"
     >
       <img
-        class="z-10 border border-dh-orange bottom-[50%] relative h-auto w-[400px]"
+        class="z-10 border border-dh-orange bottom-[50%] relative h-auto w-[400px] xl:w-full"
         style="clip-path: inset(15px 0px);"
         src="https://media.digitalhippie.xyz/beat%20tape%20cover.jpg"
         alt="a thing"
@@ -38,7 +38,7 @@
     </div>
   </div>
   <div
-    class="relative grid col-span-5 row-span-8 col-start-5 row-start-1 pl-8 pb-12"
+    class="relative grid col-span-5 row-span-8 col-start-5 xl:col-start-3 row-start-1 pl-8 pb-12"
   >
     <div class="flex flex-col row-start-2">
       <h4
