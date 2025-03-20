4ad5db82368fa42e5973dbb2723b3a5faabff69d
skywizard <modiseab+github@gmail.com>
2025-03-20 21:11:17 +0200

    moved more... stuff...

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,8 @@
+import { PUBLIC_SANITY_GQL_API } from "$env/static/public";
 import { HoudiniClient } from '$houdini';
 
 export default new HoudiniClient({
-    url: 'https://pd7aruhx.api.sanity.io/v2023-08-01/graphql/production/experiment'
-
+    url: `${PUBLIC_SANITY_GQL_API}/v2023-08-01/graphql/production/experiment`,
     // uncomment this to configure the network call (for things like authentication)
     // for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
     // fetchParams({ session }) {
