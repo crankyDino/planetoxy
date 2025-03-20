9863031e5e9f5edc8034cc0f96014a6b6f3d6936
skywizard <modiseab+github@gmail.com>
2025-03-20 21:21:34 +0200

    fix: Houdini config url

--- houdini.config.js
+++ houdini.config.js
@@ -1,11 +1,9 @@
-import { PUBLIC_SANITY_GQL_API } from "$env/static/public";
-
-/// <references types="houdini-svelte">
+// <references types="houdini-svelte">
 
 /** @type {import('houdini').ConfigFile} */
 const config = {
   watchSchema: {
-    url: `${PUBLIC_SANITY_GQL_API}/v2023-08-01/graphql/production/experiment`,
+    url: `env:PUBLIC_SANITY_GQL_API/v2023-08-01/graphql/production/experiment`,
   },
   plugins: {
     "houdini-svelte": { forceRunesMode: true },
