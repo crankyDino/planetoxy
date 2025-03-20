c0a66f6023e8614d0456d465a1f0ad2cda0d169e
skywizard <modiseab+github@gmail.com>
2025-03-20 21:08:54 +0200

    moved... stuff...

--- houdini.config.js
+++ houdini.config.js
@@ -1,9 +1,11 @@
+import { PUBLIC_SANITY_GQL_API } from "$env/static/public";
+
 /// <references types="houdini-svelte">
 
 /** @type {import('houdini').ConfigFile} */
 const config = {
   watchSchema: {
-    url: "https://pd7aruhx.api.sanity.io/v2023-08-01/graphql/production/experiment",
+    url: `${PUBLIC_SANITY_GQL_API}/v2023-08-01/graphql/production/experiment`,
   },
   plugins: {
     "houdini-svelte": { forceRunesMode: true },
