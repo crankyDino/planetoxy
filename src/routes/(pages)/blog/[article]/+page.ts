7f6cfcf4ea7f594f8365495f9bb351eb779a56a2
skywizard <modiseab+github@gmail.com>
2026-02-08 00:54:19 +0200

    test runner

--- src/routes/(pages)/blog/[article]/+page.ts
+++ src/routes/(pages)/blog/[article]/+page.ts
@@ -11,18 +11,15 @@ type TArticle = {
  * @returns 
  */
 export async function load({ params }) {
-    try {
-        console.log(params);
-        
+    try {        
         const res = await fetch(`/api/blog/content?title=${params.article}`, {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' }
         });
         
         const article: TArticle = await res.json();
-        console.log(article);
+        
         return article
-
     } catch (e) {
         error(404, `Could not find ${params.article}`)
     }
